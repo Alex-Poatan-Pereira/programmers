418d6c23a9220be26fc66e4099de0140b44dcbbc
Alex <hwaeeting0104@gmail.com>
2024-11-06 10:47:29 +0900

    [level 0] Title: 두 수의 차, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차/두 수의 차.py
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.py
@@ -1,3 +1,8 @@
 def solution(num1, num2):
+    num1 = int(num1)
+    num2 = int(num2)
+    if num1 and num2 >50000 or num1 and num2 <-50000:
+        print("숫자의 범위는 -50000이상 50000이하 입니다")
+        exit
     answer = num1 - num2
     return answer
