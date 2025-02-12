a4d78269ea03fed1420e4395d1a1b7fc5f2929f7
Alex <hwaeeting0104@gmail.com>
2025-02-12 22:41:01 +0900

    [level 1] Title: 시저 암호, Time: 3.69 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -0,0 +1,10 @@
+def solution(s, n):
+    result = ""
+    for i in s:
+        if i == " ":
+            result += " "
+        elif i.islower():
+            result += chr((ord(i)-ord('a') + n) % 26 + ord('a'))
+        elif i.isupper():
+            result += chr((ord(i)-ord('A') + n) % 26 + ord('A'))
+    return result
